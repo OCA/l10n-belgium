024a00849cc8fd9b0fd452bac93612c040f31604
Christophe Hanon <chanon@adins.com>
2015-08-28 14:23:20 +0200

    Split l10n_be_financial_reports

--- l10n_be_financial_reports/__init__.py
+++ l10n_be_financial_reports/__init__.py
@@ -20,7 +20,5 @@
 #
 ##############################################################################
 
-from . import wizard_multi_charts_accounts
-from . import res_config
-from . import account
-from . import account_fix
+from . import account_financial_report
+from . import wizard
