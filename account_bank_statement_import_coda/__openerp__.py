7ca7dc1f8677bc152b2333b100a57af5742bd8cb
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2015-11-25 23:25:49 +0100

    normalize version number

--- account_bank_statement_import_coda/__openerp__.py
+++ account_bank_statement_import_coda/__openerp__.py
@@ -27,7 +27,7 @@
     'author': "ACSONE SA/NV,Odoo Community Association (OCA)",
     'website': "http://www.acsone.eu",
     'category': 'Accounting & Finance',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'account_bank_statement_import'
