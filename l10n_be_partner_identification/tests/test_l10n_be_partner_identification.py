a538a74c6630e4f8e540d2b02f42c64c9ab0c8c0
Denis Roussel (ACSONE) <rousseldenis@users.noreply.github.com>
2017-02-13 09:30:56 +0100

    [IMP] Add Belgium ID Card support (#2)
    * [IMP] Add Belgium ID Card support

--- l10n_be_partner_identification/tests/test_l10n_be_partner_identification.py
+++ l10n_be_partner_identification/tests/test_l10n_be_partner_identification.py
@@ -29,3 +29,23 @@ class TestL10nBePartnerIdentification(common.TransactionCase):
         with self.assertRaises(ValidationError), self.env.cr.savepoint():
             # check invalid for a person born after 2000
             id_number.name = '07.01.16-234.52'
+
+    def test_validate_id_card(self):
+        partner_id_category = self.env.ref(
+            'l10n_be_partner_identification.'
+            'l10n_be_id_card_category')
+        partner_1 = self.env.ref('base.res_partner_1')
+        # born before 2000
+        partner_1.write({'id_numbers':  [(0, 0,  {
+            'name': '000-0000000-97',
+            'category_id': partner_id_category.id
+        })]})
+        id_number = partner_1.id_numbers[0]
+        self.assertEqual(id_number.name, '000-0000000-97')
+        # born after 2000
+        id_number.write({'name': '000-0000001-01',
+                         'category_id': partner_id_category.id})
+        self.assertEqual(id_number.name, '000-0000001-01')
+        with self.assertRaises(ValidationError), self.env.cr.savepoint():
+            # check invalid id number
+            id_number.name = '000-0000001-02'
