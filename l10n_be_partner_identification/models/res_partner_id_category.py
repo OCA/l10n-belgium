1c8abf9f8d5ac60d3c1ab8c0f69525cb4ff98b1b
Denis Roussel (ACSONE) <rousseldenis@users.noreply.github.com>
2017-02-13 09:30:56 +0100

    [IMP] Add Belgium ID Card support (#2)
    * [IMP] Add Belgium ID Card support

--- l10n_be_partner_identification/models/res_partner_id_category.py
+++ l10n_be_partner_identification/models/res_partner_id_category.py
@@ -38,3 +38,26 @@ class ResPartnerIdCategory(models.Model):
         if to_check != check:
             return failed
         return not failed
+
+    @api.model
+    def validate_l10n_be_id_card(self, id_number):
+        """Method called from the validation_code to validate a belgium id
+        card number
+        """
+        # replace all non digit char since on an identity card, the number is
+        # formatted as 000-00000000-97
+        num = re.sub('[^0-9]', '', id_number.name)
+        failed = True
+        if len(num) != 12:
+            return failed
+        if not num.isdigit():
+            return failed
+        prefix = int(num[0:10])
+        suffix = int(num[-2:])
+        to_check = 97 - (prefix % 97)
+
+        if (to_check == 97 and to_check == suffix) or\
+                (int(suffix) + int(to_check) == 97):
+            return not failed
+        else:
+            return failed
