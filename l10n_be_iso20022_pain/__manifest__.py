a8ebba3e3c27cc8ee576f0bc80b04814c6eb79a1
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2017-10-03 17:29:54 +0200

    [MIG] l10n_be_iso20022_pain to 10.0

--- l10n_be_iso20022_pain/__manifest__.py
+++ l10n_be_iso20022_pain/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 ACSONE SA/NV
+# Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'ISO 20022 PAIN Support for Belgium',
     'summary': """
         This module adds Belgium-specific support to account_payment_order.""",
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/l10n-belgium',
@@ -14,4 +14,5 @@
         'account_payment_order',
         'l10n_be_invoice_bba',
     ],
+    'installable': True,
 }
