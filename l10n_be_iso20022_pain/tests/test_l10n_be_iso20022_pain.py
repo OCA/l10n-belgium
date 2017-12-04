910ac1c3081682d621661f94423ea7d624c9bb65
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-12-04 10:18:10 +0100

    [FIX] l10n_be_iso20022_pain: test was really broken, fixed

--- l10n_be_iso20022_pain/tests/test_l10n_be_iso20022_pain.py
+++ l10n_be_iso20022_pain/tests/test_l10n_be_iso20022_pain.py
@@ -15,57 +15,51 @@ class TestL10nBeIso20022Pain(SavepointCase):
         # MODELS
         cls.invoice_model = cls.env['account.invoice']
         cls.account_model = cls.env['account.account']
-        cls.invoice_line_model = cls.env['account.invoice.line']
         cls.payment_line_model = cls.env['account.payment.line']
         cls.payment_mode_model = cls.env['account.payment.mode']
         cls.journal_model = cls.env['account.journal']
 
         # INSTANCES
-        # Instance: Account
-        cls.invoice_account = cls.account_model.search(
-            [('user_type_id',
-              '=',
-              cls.env.ref('account.data_account_type_receivable').id
-              )], limit=1)
-        # Instance: Invoice Line
-        cls.invoice_line = cls.invoice_line_model.create(
-            {'name': 'Test invoice line',
-             'account_id': cls.invoice_account.id,
-             'quantity': 2.000,
-             'price_unit': 2.99})
-        # Instance: Account (sales)
-        cls.account_sales = cls.account_model.create({
-            'code': "X1020",
-            'name': "Product Sales - (test)",
-            'user_type_id': cls.env.ref(
-                'account.data_account_type_revenue').id
-        })
-        # Instance: Journal
-        cls.sales_journal = cls.journal_model.create({
-            'name': "Sales Journal - (test)",
-            'code': "TSAJ",
-            'type': "sale",
-            'refund_sequence': True,
-            'default_debit_account_id': cls.account_sales.id,
-            'default_credit_account_id': cls.account_sales.id,
-        })
+        cls.payable_account = cls.account_model.search([
+            ('user_type_id',
+             '=',
+             cls.env.ref('account.data_account_type_payable').id)
+        ], limit=1)
+        cls.expense_account = cls.account_model.search([
+            ('user_type_id',
+             '=',
+             cls.env.ref('account.data_account_type_expenses').id)
+        ], limit=1)
+        cls.purchase_journal = cls.journal_model.search([
+            ('type', '=', 'purchase')
+        ], limit=1)
+        cls.bank_journal = cls.journal_model.search([
+            ('type', '=', 'bank')
+        ], limit=1)
         # Instance: Payment Mode
         cls.payment_mode = cls.payment_mode_model.create({
             'name': 'Test payment mode',
             'payment_method_id': cls.env.ref(
                 'account.account_payment_method_manual_out').id,
-            'fixed_journal_id': cls.sales_journal.id,
-            'bank_account_link': 'fixed'})
+            'fixed_journal_id': cls.bank_journal.id,
+            'bank_account_link': 'fixed',
+        })
         # Instance: Invoice
         cls.invoice = cls.invoice_model.create({
             'partner_id': cls.env.ref('base.res_partner_2').id,
-            'account_id': cls.invoice_account.id,
+            'journal_id': cls.purchase_journal.id,
+            'account_id': cls.payable_account.id,
             'type': 'in_invoice',
-            'invoice_line_ids': [(6, 0, [cls.invoice_line.id])],
+            'invoice_line_ids': [(0, 0, {
+                'name': 'Test invoice line',
+                'account_id': cls.expense_account.id,
+                'quantity': 2.000,
+                'price_unit': 2.99,
+            })],
             'reference_type': 'bba',
             'reference': '+++868/0542/73023+++',
-            'state': 'open',
-            'payment_mode_id': cls.payment_mode.id})
+            'payment_mode_id': cls.payment_mode.id
+        })
 
     def _prepare_payment_line_creation_dict(self):
         return {
@@ -85,7 +79,7 @@ class TestL10nBeIso20022Pain(SavepointCase):
         Expected result:
             - The created payment line takes the BBA communication
         """
-        self.invoice.action_move_create()
+        self.invoice.action_invoice_open()
         self.invoice.create_account_payment_line()
         pl = self.payment_line_model.search(
             [('move_line_id.invoice_id', '=', self.invoice.id)], limit=1)
