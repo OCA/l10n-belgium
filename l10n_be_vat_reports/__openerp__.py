71a40f4a949450561ee08847199f683d19796ab0
robin.keunen <robin@keunen.net>
2019-03-06 11:12:33 +0100

    [MIG] l10n_be_vat_reports: rename manifest

--- l10n_be_vat_reports/__openerp__.py
+++ l10n_be_vat_reports/__openerp__.py
@@ -28,7 +28,7 @@
               'Odoo Community Association (OCA)',
     'website': 'http://acsone.eu',
     'category': 'Reporting',
-    'version': '10.0.1.0.0',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'l10n_be',
