cdfb433003cd01a31cd344a4ed2b30356c64e7ea
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-09-19 14:17:20 +0200

    [FIX] pep8

--- account_bank_statement_import_coda/models/account_journal.py
+++ account_bank_statement_import_coda/models/account_journal.py
@@ -5,6 +5,6 @@ class AccountJournal(models.Model):
     _inherit = "account.journal"
 
     def _get_bank_statements_available_import_formats(self):
-        formats_list = super(AccountJournal, self)._get_bank_statements_available_import_formats()
+        formats_list = super()._get_bank_statements_available_import_formats()
         formats_list.append('coda')
         return formats_list
