e653b2cdeb08db98427e70de38ddce679a44f179
laurent.corron <laurent.corron@acsone.eu>
2019-11-26 14:22:33 +0100

    [MIG] l10n_be_partner_identification/: Migration to 13.0

--- l10n_be_partner_identification/__manifest__.py
+++ l10n_be_partner_identification/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # © 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Belgium Partner Identification Numbers",
     "category": "Customer Relationship Management",
-    "version": "10.0.1.0.0",
-    "depends": ["partner_identification",],
-    "data": ["data/l10n_be_partner_identification.xml",],
+    "version": "13.0.1.0.0",
+    "depends": ["partner_identification"],
+    "data": ["data/l10n_be_partner_identification.xml"],
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://odoo-community.org/",
     "license": "AGPL-3",
--- l10n_be_partner_identification/tests/__init__.py
+++ l10n_be_partner_identification/tests/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import test_l10n_be_partner_identification
