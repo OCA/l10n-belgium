f469878c28041b41c17843507021e4a9df8e01df
robin.keunen <robin@keunen.net>
2020-02-19 18:49:52 +0100

    [MIG] l10n_be_vat_reports: 12.0

--- l10n_be_vat_reports/__manifest__.py
+++ l10n_be_vat_reports/__manifest__.py
@@ -28,7 +28,7 @@
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "http://acsone.eu",
     "category": "Reporting",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["l10n_be", "account"],
     "data": [
