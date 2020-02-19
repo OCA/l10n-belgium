16df741a28605d02c7cb2fce5dc82d8494701448
robin.keunen <robin@keunen.net>
2020-02-19 18:35:42 +0100

    [MIG] l10n_be_vat_reports: 11.0

--- l10n_be_vat_reports/__manifest__.py
+++ l10n_be_vat_reports/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #     This file is part of l10n_be_mis_reports,
@@ -29,7 +28,7 @@
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "http://acsone.eu",
     "category": "Reporting",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["l10n_be", "account"],
     "data": [
