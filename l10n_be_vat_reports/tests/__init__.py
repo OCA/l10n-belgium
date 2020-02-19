f976d45706140eccfbec5c110c19b86c6209d525
robin.keunen <robin@keunen.net>
2020-02-19 18:29:55 +0100

    [ADD] l10n_be_vat_reports: credits to Odoo SA

--- l10n_be_vat_reports/tests/__init__.py
+++ l10n_be_vat_reports/tests/__init__.py
@@ -4,6 +4,8 @@
 #
 #    Authors: Adrien Peiffer
 #    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
