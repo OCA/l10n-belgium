916af309b0b9d4f40b14bb1c681e9dca67883a9c
robin.keunen <robin@keunen.net>
2020-03-06 13:49:10 +0100

    [MIG] l10n_be_vat_reports: Partner VAT Intra

--- l10n_be_vat_reports/tests/test_vat_intra.py
+++ l10n_be_vat_reports/tests/test_vat_intra.py
@@ -1,10 +1,9 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.addons.report.models import report
 
 from lxml.etree import XML
-import unittest
+import base64
 import time
 
 from .common import TestVatReportsCommon
@@ -21,13 +20,14 @@ class TestVatIntra(TestVatReportsCommon):
         wizard = self.env["partner.vat.intra"].create(
             {
                 "period_code": time.strftime("00%Y"),
-                "date_start": time.strftime("%Y-01-01"),  # todo use native date
+                "date_start": time.strftime("%Y-01-01"),
                 "date_end": time.strftime("%Y-12-31"),
             }
         )
+        wizard.get_partners()
         wizard.create_xml()
         ns = {"ns2": "http://www.minfin.fgov.be/IntraConsignment"}
-        xml = XML(wizard.file_save.decode("base64"))
+        xml = XML(base64.b64decode(wizard.file_save))
         xml_vat_amount = xml.xpath("//ns2:IntraListing", namespaces=ns)[
             0
         ].attrib["AmountSum"]
@@ -38,19 +38,15 @@ class TestVatIntra(TestVatReportsCommon):
             self.assertTrue(code.text)
         self.assertEqual("450.00", xml_vat_amount)
 
-    @unittest.skipIf(
-        report.wkhtmltopdf_state == "install", "wkhtmltopdf not available"
-    )
     def test_pdf_list(self):
         wizard = self.env["partner.vat.intra"].create(
             {
                 "period_code": time.strftime("00%Y"),
-                "date_start": time.strftime("%Y-01-01"),  # todo use native date
+                "date_start": time.strftime("%Y-01-01"),
                 "date_end": time.strftime("%Y-12-31"),
             }
         )
-        # todo  If you need to generate a PDF in tests, you have to pass the context key force_report_rendering=True for getting that instead of an HTML, as now the method qweb_render_pdf defaults to qweb_render_html on test environment.
-        report_action = wizard.preview()
-        self.env["report"].get_pdf(
-            [], report_action["report_name"], data=report_action["data"]
-        )
+        wizard.get_partners()
+        self.assertEquals(wizard.client_ids[0].amount, 450.0)
+        self.assertEquals(wizard.amount_total, 450.0)
+        wizard.print_vat_intra()
