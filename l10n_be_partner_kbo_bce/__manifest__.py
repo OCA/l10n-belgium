7953c8f9f2d6f088362af8e36ba66379c4a9c30d
Luc De Meyer <luc.demeyer@noviat.com>
2020-03-23 14:08:29 +0100

    13.0 port

---
+++ l10n_be_partner_kbo_bce/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2009-2020 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Belgium - KBO/BCE numbers",
+    "category": "Localization",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Noviat",
+    "website": "https://www.noviat.com",
+    "depends": ["partner_identification", "base_vat_sanitized"],
+    "data": ["data/res_partner_id_category.xml", "views/res_partner.xml"],
+    "demo": ["demo/res_partner.xml"],
+    "installable": True,
+}
