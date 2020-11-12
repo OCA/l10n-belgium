1d4e10a4a0cfd613e4b229931b71631012f32d99
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-11-12 15:14:47 +0100

    [MIG] l10n_be_iso20022_pain: run pre-commit

--- l10n_be_iso20022_pain/tests/test_l10n_be_iso20022_pain.py
+++ l10n_be_iso20022_pain/tests/test_l10n_be_iso20022_pain.py
@@ -1,72 +1,89 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
 from odoo.exceptions import ValidationError
+from odoo.tests.common import SavepointCase
 
 
 class TestL10nBeIso20022Pain(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super(TestL10nBeIso20022Pain, cls).setUpClass()
 
         # MODELS
-        cls.invoice_model = cls.env['account.invoice']
-        cls.account_model = cls.env['account.account']
-        cls.payment_line_model = cls.env['account.payment.line']
-        cls.payment_mode_model = cls.env['account.payment.mode']
-        cls.journal_model = cls.env['account.journal']
+        cls.invoice_model = cls.env["account.invoice"]
+        cls.account_model = cls.env["account.account"]
+        cls.payment_line_model = cls.env["account.payment.line"]
+        cls.payment_mode_model = cls.env["account.payment.mode"]
+        cls.journal_model = cls.env["account.journal"]
 
         # INSTANCES
-        cls.payable_account = cls.account_model.search([
-            ('user_type_id',
-             '=',
-             cls.env.ref('account.data_account_type_payable').id)
-        ], limit=1)
-        cls.expense_account = cls.account_model.search([
-            ('user_type_id',
-             '=',
-             cls.env.ref('account.data_account_type_expenses').id)
-        ], limit=1)
-        cls.purchase_journal = cls.journal_model.search([
-            ('type', '=', 'purchase')
-        ], limit=1)
-        cls.bank_journal = cls.journal_model.search([
-            ('type', '=', 'bank')
-        ], limit=1)
+        cls.payable_account = cls.account_model.search(
+            [
+                (
+                    "user_type_id",
+                    "=",
+                    cls.env.ref("account.data_account_type_payable").id,
+                )
+            ],
+            limit=1,
+        )
+        cls.expense_account = cls.account_model.search(
+            [
+                (
+                    "user_type_id",
+                    "=",
+                    cls.env.ref("account.data_account_type_expenses").id,
+                )
+            ],
+            limit=1,
+        )
+        cls.purchase_journal = cls.journal_model.search(
+            [("type", "=", "purchase")], limit=1
+        )
+        cls.bank_journal = cls.journal_model.search([("type", "=", "bank")], limit=1)
         # Instance: Payment Mode
-        cls.payment_mode = cls.payment_mode_model.create({
-            'name': 'Test payment mode',
-            'payment_method_id': cls.env.ref(
-                'account.account_payment_method_manual_out').id,
-            'fixed_journal_id': cls.bank_journal.id,
-            'bank_account_link': 'fixed',
-        })
+        cls.payment_mode = cls.payment_mode_model.create(
+            {
+                "name": "Test payment mode",
+                "payment_method_id": cls.env.ref(
+                    "account.account_payment_method_manual_out"
+                ).id,
+                "fixed_journal_id": cls.bank_journal.id,
+                "bank_account_link": "fixed",
+            }
+        )
         # Instance: Invoice
-        cls.invoice = cls.invoice_model.create({
-            'partner_id': cls.env.ref('base.res_partner_2').id,
-            'journal_id': cls.purchase_journal.id,
-            'account_id': cls.payable_account.id,
-            'type': 'in_invoice',
-            'invoice_line_ids': [(0, 0, {
-                'name': 'Test invoice line',
-                'account_id': cls.expense_account.id,
-                'quantity': 2.000,
-                'price_unit': 2.99,
-            })],
-            'reference_type': 'bba',
-            'reference': '+++868/0542/73023+++',
-            'payment_mode_id': cls.payment_mode.id
-        })
+        cls.invoice = cls.invoice_model.create(
+            {
+                "partner_id": cls.env.ref("base.res_partner_2").id,
+                "journal_id": cls.purchase_journal.id,
+                "account_id": cls.payable_account.id,
+                "type": "in_invoice",
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Test invoice line",
+                            "account_id": cls.expense_account.id,
+                            "quantity": 2.000,
+                            "price_unit": 2.99,
+                        },
+                    )
+                ],
+                "reference_type": "bba",
+                "reference": "+++868/0542/73023+++",
+                "payment_mode_id": cls.payment_mode.id,
+            }
+        )
 
     def _prepare_payment_line_creation_dict(self):
         return {
-            'currency_id': self.env.ref('base.EUR').id,
-            'partner_id': self.env.ref('base.res_partner_2').id,
-            'communication_type': 'bba',
-            'amount_currency': 123.321,
+            "currency_id": self.env.ref("base.EUR").id,
+            "partner_id": self.env.ref("base.res_partner_2").id,
+            "communication_type": "bba",
+            "amount_currency": 123.321,
         }
 
     def test_create_account_payment_line_01(self):
@@ -82,8 +99,9 @@ class TestL10nBeIso20022Pain(SavepointCase):
         self.invoice.action_invoice_open()
         self.invoice.create_account_payment_line()
         pl = self.payment_line_model.search(
-            [('move_line_id.invoice_id', '=', self.invoice.id)], limit=1)
-        self.assertEqual(pl.communication, '868054273023')
+            [("move_line_id.invoice_id", "=", self.invoice.id)], limit=1
+        )
+        self.assertEqual(pl.communication, "868054273023")
 
     def test_create_account_payment_line_02(self):
         """
@@ -95,7 +113,7 @@ class TestL10nBeIso20022Pain(SavepointCase):
             - ValidationError
         """
         vals = self._prepare_payment_line_creation_dict()
-        vals['communication'] = '868054273024'
+        vals["communication"] = "868054273024"
         with self.assertRaises(ValidationError):
             self.payment_line_model.create(vals)
 
@@ -110,7 +128,7 @@ class TestL10nBeIso20022Pain(SavepointCase):
             - ValidationError
         """
         vals = self._prepare_payment_line_creation_dict()
-        vals['communication'] = '8680542730241'
+        vals["communication"] = "8680542730241"
         with self.assertRaises(ValidationError):
             self.payment_line_model.create(vals)
 
@@ -125,7 +143,7 @@ class TestL10nBeIso20022Pain(SavepointCase):
             - ValidationError
         """
         vals = self._prepare_payment_line_creation_dict()
-        vals['communication'] = '86805427302'
+        vals["communication"] = "86805427302"
         with self.assertRaises(ValidationError):
             self.payment_line_model.create(vals)
 
@@ -139,5 +157,5 @@ class TestL10nBeIso20022Pain(SavepointCase):
             - The payment line is created with the valid communication
         """
         vals = self._prepare_payment_line_creation_dict()
-        vals['communication'] = '868054273023'
+        vals["communication"] = "868054273023"
         self.payment_line_model.create(vals)
