1e86488fffd1e3dc90b3a6776704a7f51628a424
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-03-03 08:38:49 +0100

    [ADD] companyweb_base

---
+++ companyweb_base/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2021 ACSONE SA/NV
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "companyweb_base",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-belgium",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "installable": True,
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/partner_view.xml",
+        "wizards/credential_wizard.xml",
+    ],
+    "external_dependencies": {
+        "python": [
+            "zeep",
+        ],
+    },
+    "depends": ["web_notify"],
+    "maintainers": ["xavier-bouquiaux"],
+}
