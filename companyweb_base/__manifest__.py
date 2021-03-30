c34c5c03795842110bbbf4c2e17c9af37ccb49ea
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-03-30 11:42:58 +0200

    [LIC] companyweb_base: use AGPL
    We use AGPL for compatibility with web_notify which is AGPL too.
    We may change back to LGPL if web_notify changes too.

--- companyweb_base/__manifest__.py
+++ companyweb_base/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2021 ACSONE SA/NV
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "companyweb_base",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-belgium",
     "version": "14.0.1.0.1",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "data": [
         "security/security.xml",
--- companyweb_base/wizards/credential_wizard.py
+++ companyweb_base/wizards/credential_wizard.py
@@ -1,4 +1,5 @@
 # Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
 
