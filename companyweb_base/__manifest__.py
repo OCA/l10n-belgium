a591efff3aab6263bde32fab9422d31c36367b62
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-03-30 12:15:14 +0200

    [IMP] companyweb_base: cover image and summary

--- companyweb_base/__manifest__.py
+++ companyweb_base/__manifest__.py
@@ -1,7 +1,11 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "companyweb_base",
+    "name": "Companyweb",
+    "summary": (
+        "Know who you are dealing with. "
+        "Enhance Odoo partner data from companyweb.be."
+    ),
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-belgium",
     "version": "14.0.1.0.2",
@@ -12,6 +16,9 @@
         "security/ir.model.access.csv",
         "views/partner_view.xml",
         "wizards/credential_wizard.xml",
+    ],
+    "images": [
+        "static/description/main_screenshot.png",
     ],
     "external_dependencies": {
         "python": [
