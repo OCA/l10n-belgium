81fb1e5478e9a996a82bcc3f7beae4e6864b01a0
João Marques <joao.marques@tecnativa.com>
2021-08-31 13:15:59 +0100

    [MIG] l10n_be_mis_reports: Migration to 14.0

--- l10n_be_mis_reports/__manifest__.py
+++ l10n_be_mis_reports/__manifest__.py
@@ -5,10 +5,10 @@
     "summary": """
         MIS Builder templates for the Belgium P&L,
         Balance Sheets and VAT Declaration""",
-    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-belgium",
     "category": "Reporting",
-    "version": "13.0.1.0.2",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["mis_builder", "l10n_be"],  # OCA/account-financial-reporting
     "data": [
