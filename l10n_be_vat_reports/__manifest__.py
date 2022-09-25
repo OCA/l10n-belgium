476636d3d8f9dbb435147852d29707475ffd0f81
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-09-25 13:19:03 +0200

    Apply pre-commit

--- l10n_be_vat_reports/__manifest__.py
+++ l10n_be_vat_reports/__manifest__.py
@@ -8,7 +8,7 @@
     "Coop IT Easy SCRLfs, "
     "Odoo SA, "
     "Odoo Community Association (OCA)",
-    "website": "http://acsone.eu",
+    "website": "https://github.com/OCA/l10n-belgium",
     "category": "Reporting",
     "version": "11.0.1.0.1",
     "license": "AGPL-3",
