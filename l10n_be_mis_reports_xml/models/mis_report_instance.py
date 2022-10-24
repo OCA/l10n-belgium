3ef6ecaee1f5205f3b0ccf27a09ed24725b89f08
hugues de keyzer <odoo@hugues.info>
2022-10-24 15:08:19 +0200

    [IMP] l10n_be_mis_reports_xml: black, isort, prettier

--- l10n_be_mis_reports_xml/models/mis_report_instance.py
+++ l10n_be_mis_reports_xml/models/mis_report_instance.py
@@ -9,17 +9,13 @@ from odoo import api, fields, models
 class MisReportInstance(models.Model):
     _inherit = "mis.report.instance"
 
-    _is_be_vat_declaration = fields.Boolean(
-        compute="_compute_is_be_vat_declaration"
-    )
+    _is_be_vat_declaration = fields.Boolean(compute="_compute_is_be_vat_declaration")
 
     @api.multi
     def _compute_is_be_vat_declaration(self):
         mr_template = self.env.ref("l10n_be_mis_reports.mis_report_vat")
         for instance in self:
-            instance._is_be_vat_declaration = (
-                instance.report_id == mr_template
-            )
+            instance._is_be_vat_declaration = instance.report_id == mr_template
 
     def export_xml(self):
         self.ensure_one()
--- l10n_be_mis_reports_xml/tests/test_l10n_be_mis_reports_xml.py
+++ l10n_be_mis_reports_xml/tests/test_l10n_be_mis_reports_xml.py
@@ -3,8 +3,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
-from odoo.tests.common import TransactionCase
 from datetime import date
+
+from odoo.tests.common import TransactionCase
 
 
 class TestXMLReports(TransactionCase):
