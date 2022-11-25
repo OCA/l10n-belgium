fe371ebd4477f19193c467175623be98824678c9
hugues de keyzer <odoo@hugues.info>
2022-11-25 17:06:08 +0100

    [MIG] l10n_be_vat_reports: migration to 13.0

--- l10n_be_vat_reports/wizard/partner_vat_list.py
+++ l10n_be_vat_reports/wizard/partner_vat_list.py
@@ -30,10 +30,9 @@ class PartnerVATList(models.TransientModel):
     total_vat = fields.Float("Total VAT", compute="_compute_totals")
     comments = fields.Text("Comments")
 
-    @api.multi
     def _compute_declarant_reference(self):
         self.env["ir.sequence"].next_by_code("declarantnum")
-        company = self.env.user.company_id
+        company = self.env.company
         company_vat = company.partner_id.vat
 
         if not company_vat:
@@ -44,7 +43,6 @@ class PartnerVATList(models.TransientModel):
             seq_declarantnum = self.env["ir.sequence"].next_by_code("declarantnum")
             listing.declarant_reference = company_vat[2:] + seq_declarantnum[-4:]
 
-    @api.multi
     @api.depends("partner_ids")
     def _compute_totals(self):
         for vat_list in self:
@@ -55,88 +53,89 @@ class PartnerVATList(models.TransientModel):
                 sum(p.vat_amount for p in vat_list.partner_ids), 2
             )
 
-    @api.multi
     def get_partners(self):
         self.ensure_one()
-        date_start = date(int(self.year), 1, 1)
-        date_stop = date(int(self.year), 12, 31)
+        date_from = date(int(self.year), 1, 1)
+        date_to = date(int(self.year), 12, 31)
 
-        partners = self.env["partner.vat.list.client"].browse([])
-        be_partners = self.env["res.partner"].search([("vat", "ilike", "BE%")])
-        if not be_partners:
-            raise UserError(_("No belgium contact with a VAT number in your database."))
+        partner_vat_list_client_model = self.env["partner.vat.list.client"]
+        partners = partner_vat_list_client_model.browse([])
+        turnover_tags = ("00", "01", "02", "03", "45", "49")
+        vat_tags = ("54", "64")
         query = """
-    WITH turnover_tags AS
-      (SELECT tagsrel.account_tax_id
-       FROM account_tax_account_tag tagsrel
-       INNER JOIN ir_model_data tag_xmlid ON (
-           tag_xmlid.model = 'account.account.tag'
-           AND tagsrel.account_account_tag_id = tag_xmlid.res_id)
-       WHERE tag_xmlid.NAME IN ('tax_tag_00',
-                                'tax_tag_01',
-                                'tax_tag_02',
-                                'tax_tag_03',
-                                'tax_tag_45',
-                                'tax_tag_49'))
-    , vat_amount_tags AS
-      (SELECT tagsrel.account_tax_id
-       FROM account_tax_account_tag tagsrel
-       INNER JOIN ir_model_data tag_xmlid ON (
-           tag_xmlid.model = 'account.account.tag'
-           AND tagsrel.account_account_tag_id = tag_xmlid.res_id)
-       WHERE tag_xmlid.NAME IN ('tax_tag_54',
-                                'tax_tag_64'))
-    SELECT sub1.NAME,
-           sub1.vat,
-           ROUND(COALESCE(sub1.turnover, 0), 2) AS turnover,
-           ROUND(COALESCE(sub2.vat_amount, 0), 2) AS vat_amount
-    FROM
-      (SELECT l.partner_id,
-              p.NAME,
-              p.vat,
-              Sum(-l.balance) AS turnover
-       FROM account_move_line l
-       LEFT JOIN res_partner p ON l.partner_id = p.id
-       WHERE l.partner_id IN %s
-             AND l.date BETWEEN %s AND %s
-             AND EXISTS
-               (SELECT 1
-                FROM account_move_line_account_tax_rel taxrel
-                WHERE taxrel.account_move_line_id = l.id
-                  AND taxrel.account_tax_id IN
-                    (SELECT account_tax_id
-                     FROM turnover_tags) )
-       GROUP BY l.partner_id,
-                p.NAME,
-                p.vat) AS sub1
-    LEFT JOIN
-      (SELECT l2.partner_id,
-              Sum(-l2.balance) AS vat_amount
-       FROM account_move_line l2
-       WHERE l2.partner_id IN %s
-             AND l2.date BETWEEN %s AND %s
-             AND EXISTS
-               (SELECT 1
-                FROM vat_amount_tags
-                WHERE account_tax_id = l2.tax_line_id)
-       GROUP BY partner_id) AS sub2 ON sub1.partner_id = sub2.partner_id;
-            """
-        args = (
-            tuple(be_partners.ids),
-            date_start.isoformat(),
-            date_stop.isoformat(),
-            tuple(be_partners.ids),
-            date_start.isoformat(),
-            date_stop.isoformat(),
-        )
+with turnover_tag as (
+    select aat.id
+    from account_account_tag as aat
+    inner join account_tax_report_line_tags_rel as atrltr on
+        atrltr.account_account_tag_id = aat.id
+    inner join account_tax_report_line as atrl on
+        atrltr.account_tax_report_line_id = atrl.id and
+        atrl.tag_name in %(turnover_tags)s),
+vat_tag as (
+    select aat.id
+    from account_account_tag as aat
+    inner join account_tax_report_line_tags_rel as atrltr on
+        atrltr.account_account_tag_id = aat.id
+    inner join account_tax_report_line as atrl on
+        atrltr.account_tax_report_line_id = atrl.id and
+        atrl.tag_name in %(vat_tags)s)
+select
+    rp.name,
+    rp.vat,
+    aml1.total_amount as turnover,
+    coalesce(aml2.total_amount, 0.00) as vat_amount
+from
+    res_partner as rp
+inner join (
+    select
+        aml.partner_id,
+        round(sum(-aml.balance), 2) as total_amount
+    from account_move_line as aml
+    where aml.date between %(date_from)s and %(date_to)s
+    and exists (
+        select 1
+        from account_account_tag_account_move_line_rel as aatamlr
+        inner join turnover_tag as tt on
+            aatamlr.account_account_tag_id = tt.id
+        where aatamlr.account_move_line_id = aml.id)
+    group by 1) as aml1 on
+    aml1.partner_id = rp.id
+left join (
+    select
+        aml.partner_id,
+        round(sum(-aml.balance), 2) as total_amount
+    from account_move_line as aml
+    where aml.date between %(date_from)s and %(date_to)s
+    and exists (
+        select 1
+        from account_tax as at
+        inner join account_tax_repartition_line as atrl on
+            at.id in (atrl.invoice_tax_id, atrl.refund_tax_id)
+        inner join account_account_tag_account_tax_repartition_line_rel
+            as aatatrlr on
+            aatatrlr.account_tax_repartition_line_id = atrl.id
+        inner join vat_tag as vt on
+            aatatrlr.account_account_tag_id = vt.id
+        where at.id = aml.tax_line_id)
+    group by 1) as aml2 on
+    aml2.partner_id = rp.id
+where
+    rp.active and
+    rp.vat ilike 'be%%' and
+    aml1.total_amount >= %(limit_amount)s
+        """
+        args = {
+            "turnover_tags": turnover_tags,
+            "vat_tags": vat_tags,
+            "date_from": date_from,
+            "date_to": date_to,
+            "limit_amount": self.limit_amount,
+        }
         self.env.cr.execute(query, args)
-        seq = 0
-        for record in self.env.cr.dictfetchall():
+        for seq, record in enumerate(self.env.cr.dictfetchall(), start=1):
             record["vat"] = record["vat"].replace(" ", "").upper()
-            if record["turnover"] >= self.limit_amount:
-                seq += 1
-                record["seq"] = seq
-                partners |= self.env["partner.vat.list.client"].create(record)
+            record["seq"] = seq
+            partners |= partner_vat_list_client_model.create(record)
 
         if not partners:
             raise UserError(_("No data found for the selected year."))
@@ -151,7 +150,7 @@ class PartnerVATList(models.TransientModel):
         resource_id = model_datas.res_id
         self.partner_ids = partners.ids
         return {
-            "name": _("Vat Listing"),
+            "name": _("VAT Listing"),
             "res_id": self.id,
             "view_type": "form",
             "view_mode": "form",
@@ -161,14 +160,12 @@ class PartnerVATList(models.TransientModel):
             "target": "inline",
         }
 
-    @api.multi
     def create_xml(self):
         self.ensure_one()
         return self.env.ref(
             "l10n_be_vat_reports.l10n_be_vat_listing_consignment_xml_report"
         ).report_action(self, config=False)
 
-    @api.multi
     def print_vatlist(self):
         self.ensure_one()
 
