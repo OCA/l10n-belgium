59db9b3cdd72f065d9fa7592ca439ecba82ea0d6
Xavier Bouquiaux <xavier.bouquiaux@acsone.eu>
2022-12-02 15:11:21 +0100

    [MIG][16.0] companyweb
    companyweb's module migration to 16

--- companyweb_payment_info/__manifest__.py
+++ companyweb_payment_info/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": ("Send your customer bill to companyweb"),
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-belgium",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.1",
     "development_status": "Production/Stable",
     "license": "AGPL-3",
     "installable": True,
