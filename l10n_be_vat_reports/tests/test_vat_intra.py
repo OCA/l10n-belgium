d7246551372ffa2427c5262e1b1ef3e31778186d
hugues de keyzer <odoo@hugues.info>
2022-12-07 18:01:34 +0100

    [REF] use xml report for vat intra xml file
    * use an xml report to generate vat intra xml file instead of generating
      strings by code. this also fixes the report file name that had a .svg
      extension.
    * move common code between reports to vat.declaration.mixin.
    * remove the representative tag in the vat intra xml file.
    * sanitize declarant phone number.
    * fix some copyright headers.

--- l10n_be_vat_reports/tests/test_vat_intra.py
+++ l10n_be_vat_reports/tests/test_vat_intra.py
@@ -2,18 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-import base64
 import time
-
-from lxml.etree import XML
 
 from .common import TestVatReportsCommon
 
 
 class TestVatIntra(TestVatReportsCommon):
     def _get_tax(self):
-        company = self.env.company
-        return self.env.ref("l10n_be.%s_attn_VAT-OUT-00-EU-S" % company.id)
+        return self.env.ref("l10n_be.%s_attn_VAT-OUT-00-EU-S" % self.env.company.id)
 
     def setUp(self):
         super().setUp()
@@ -28,16 +24,15 @@ class TestVatIntra(TestVatReportsCommon):
             }
         )
         wizard.get_partners()
-        wizard.create_xml()
+        xml = self._get_xml_from_report_action(wizard.create_xml())
         ns = {"ns2": "http://www.minfin.fgov.be/IntraConsignment"}
-        xml = XML(base64.b64decode(wizard.file_save))
         xml_vat_amount = xml.xpath("//ns2:IntraListing", namespaces=ns)[0].attrib[
             "AmountSum"
         ]
         codes = xml.xpath("//ns2:IntraListing/ns2:IntraClient/ns2:Code", namespaces=ns)
         for code in codes:
             self.assertTrue(code.text)
-        self.assertEqual("450.00", xml_vat_amount)
+        self.assertEqual(xml_vat_amount, "450.00")
 
     def test_pdf_list(self):
         wizard = self.env["partner.vat.intra"].create(
@@ -54,7 +49,8 @@ class TestVatIntra(TestVatReportsCommon):
 
     def test_intra_ignore_invalid_invoices(self):
         invoice_tax = self._get_tax()
-        draft_invoice = self._create_test_invoice(invoice_tax)
+        # create a draft invoice
+        self._create_test_invoice(invoice_tax)
         cancelled_invoice = self._create_test_invoice(invoice_tax)
         cancelled_invoice.button_cancel()
         wizard = self.env["partner.vat.intra"].create(
--- l10n_be_vat_reports/tests/test_vat_reports.py
+++ l10n_be_vat_reports/tests/test_vat_reports.py
@@ -8,12 +8,10 @@ from .common import TestVatReportsCommon
 
 class TestVatListing(TestVatReportsCommon):
     def _get_tax(self):
-        company = self.env.company
-        return self.env.ref("l10n_be.%s_attn_VAT-OUT-21-S" % company.id)
+        return self.env.ref("l10n_be.%s_attn_VAT-OUT-21-S" % self.env.company.id)
 
     def setUp(self):
         super().setUp()
-        company = self.env.company
         self._create_test_data(self._get_tax())
 
     def test_partner_vat_listing(self):
@@ -30,9 +28,24 @@ class TestVatListing(TestVatReportsCommon):
         partner_vat_list.create_xml()
         partner_vat_list.print_vatlist()
 
+    def test_xml_list(self):
+        year = fields.Date.today().year
+        partner_vat_list = self.env["partner.vat.list"].create(
+            {"year": year, "limit_amount": 1}
+        )
+        partner_vat_list.get_partners()
+        xml = self._get_xml_from_report_action(partner_vat_list.create_xml())
+        ns = {"ns2": "http://www.minfin.fgov.be/ClientListingConsignment"}
+        node = xml.xpath("//ns2:ClientListing", namespaces=ns)[0]
+        xml_total_turnover = node.attrib["TurnOverSum"]
+        xml_total_vat = node.attrib["VATAmountSum"]
+        self.assertEqual(xml_total_turnover, "450.00")
+        self.assertEqual(xml_total_vat, "94.50")
+
     def test_vat_listing_ignore_invalid_invoices(self):
         invoice_tax = self._get_tax()
-        draft_invoice = self._create_test_invoice(invoice_tax)
+        # create a draft invoice
+        self._create_test_invoice(invoice_tax)
         cancelled_invoice = self._create_test_invoice(invoice_tax)
         cancelled_invoice.button_cancel()
         year = fields.Date.today().year
