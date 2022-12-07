d76315dcdfd3b20f85b6d2c3618f2d4bd11f8909
hugues de keyzer <odoo@hugues.info>
2022-12-07 10:27:38 +0100

    [FIX] fix vat report queries
    * ignore account.moves not in "posted" state.
    * vat client listing: account only for account.move.lines linked to the
    current company.
    * vat client listing: take inactive res.partners into account.

--- l10n_be_vat_reports/tests/test_vat_reports.py
+++ l10n_be_vat_reports/tests/test_vat_reports.py
@@ -7,11 +7,14 @@ from .common import TestVatReportsCommon
 
 
 class TestVatListing(TestVatReportsCommon):
+    def _get_tax(self):
+        company = self.env.company
+        return self.env.ref("l10n_be.%s_attn_VAT-OUT-21-S" % company.id)
+
     def setUp(self):
         super().setUp()
         company = self.env.company
-        tax = self.env.ref("l10n_be.%s_attn_VAT-OUT-21-S" % company.id)
-        self._create_test_data(tax)
+        self._create_test_data(self._get_tax())
 
     def test_partner_vat_listing(self):
         year = fields.Date.today().year
@@ -26,3 +29,29 @@ class TestVatListing(TestVatReportsCommon):
         # coverage
         partner_vat_list.create_xml()
         partner_vat_list.print_vatlist()
+
+    def test_vat_listing_ignore_invalid_invoices(self):
+        invoice_tax = self._get_tax()
+        draft_invoice = self._create_test_invoice(invoice_tax)
+        cancelled_invoice = self._create_test_invoice(invoice_tax)
+        cancelled_invoice.button_cancel()
+        year = fields.Date.today().year
+        partner_vat_list = self.env["partner.vat.list"].create(
+            {"year": year, "limit_amount": 1}
+        )
+        partner_vat_list.get_partners()
+        # draft and cancelled invoices must be ignored
+        self.assertEqual(partner_vat_list.total_turnover, 450.0)
+        self.assertEqual(partner_vat_list.total_vat, 94.5)
+
+    def test_vat_listing_include_archived_partners(self):
+        self.partner.active = False
+        # ensure change is done in the database
+        self.partner.flush()
+        year = fields.Date.today().year
+        partner_vat_list = self.env["partner.vat.list"].create(
+            {"year": year, "limit_amount": 1}
+        )
+        partner_vat_list.get_partners()
+        self.assertEqual(partner_vat_list.total_turnover, 450.0)
+        self.assertEqual(partner_vat_list.total_vat, 94.5)
