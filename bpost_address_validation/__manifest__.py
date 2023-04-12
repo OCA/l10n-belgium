eb1706a74be1f9cbb8b88fc58b92201506716d9c
Samuel Kouff <samuel.kouff@hotmail.com>
2023-04-12 14:30:07 +0200

    [ADD] l10n_be_bpost_address_validation: init

---
+++ bpost_address_validation/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Bpost address validation",
+    "version": "16.0.1.0.0",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-belgium",
+    "depends": ["base", "web"],
+    "assets": {
+        "web.assets_backend": [
+            "bpost_adress_validation/static/src/js/**/*",
+            "web/static/src/libs/fontawesome/css/font-awesome.css",
+            ("include", "web._assets_helpers"),
+            "web/static/src/scss/pre_variables.scss",
+            "web/static/lib/bootstrap/scss/_variables.scss",
+            ("include", "web._assets_bootstrap"),
+        ]
+    },
+    "installable": True,
+}
