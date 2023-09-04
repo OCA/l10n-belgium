a6085c8c06d553ca255ecb90cfdcc63b3f9e0261
Denis Roussel <denis.roussel@acsone.eu>
2023-09-04 10:54:24 +0200

    [IMP] l10n_be_eco_tax: pre-commit stuff

--- l10n_be_eco_tax/__manifest__.py
+++ l10n_be_eco_tax/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 BCIM sprl(http://www.bcim.be)
 # Copyright 2017 Okia SPRL (https://okia.be)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -7,19 +6,16 @@
     "name": "Belgium Eco Taxes",
     "summary": "Data module to support BEBAT and RECUPEL taxes",
     "version": "10.0.1.0.0",
-    "author": "ACSONE SA/NV,"
-              "BCIM sprl, "
-              "Okia, "
-              "Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV," "BCIM sprl, " "Okia, " "Odoo Community Association (OCA)",
     "category": "Generic Modules/Accounting",
-    "website": "http://odoo-community.org",
-    "depends": ['account', 'l10n_be'],
+    "website": "https://github.com/OCA/l10n-belgium",
+    "depends": ["account", "l10n_be"],
     "data": [
-        'data/account_tax_code_template.xml',
-        'data/account_account_template_data.xml',
-        'data/account_tax_template_recupel_out_data.xml',
-        'data/account_tax_template_recupel_in_data.xml',
-        'data/account_tax_template_bebat_data.xml',
+        "data/account_tax_code_template.xml",
+        "data/account_account_template_data.xml",
+        "data/account_tax_template_recupel_out_data.xml",
+        "data/account_tax_template_recupel_in_data.xml",
+        "data/account_tax_template_bebat_data.xml",
     ],
     "license": "AGPL-3",
     "installable": True,
