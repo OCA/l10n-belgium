1e0499b9705d7d8b7cf0e1e60a3cebb57246fa69
Xavier Bouquiaux <xavier.bouquiaux@gmail.com>
2025-03-06 09:03:52 +0100

    company_web : credentials are now editable

--- companyweb_base/__manifest__.py
+++ companyweb_base/__manifest__.py
@@ -16,6 +16,7 @@
         "security/security.xml",
         "security/ir.model.access.csv",
         "views/partner_view.xml",
+        "views/res_users_view.xml",
         "wizards/credential_wizard.xml",
     ],
     "images": [
