4725238ca2f5c8a9c9567a35219e1238066450d5
Simon Hick <sim@coopiteasy.be>
2025-10-07 17:11:07 +0200

    [ADD] l10n_be_partner_company_type

---
+++ l10n_be_partner_company_type/__manifest__.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2025 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Belgium Company Types",
+    "summary": "Data Module to add belgian company types as given in"
+    "https://www.belgium.be/fr/economie/entreprise/creation/types_de_societe",
+    "version": "16.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://github.com/OCA/l10n-belgium",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "maintainers": ["mihien"],
+    "license": "AGPL-3",
+    "depends": [
+        "partner_company_type",
+    ],
+    "data": [
+        "data/company_type.xml",
+    ],
+}
---
+++ l10n_be_partner_company_type/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Data Module to add belgian company types as given in https://www.belgium.be/fr/economie/entreprise/creation/types_de_societe.
