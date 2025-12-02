5b865c12977d4c460ee9937275b89cb21dc62f4c
Ariel Barreiros <50119000+arielbarreiros96@users.noreply.github.com>
2025-12-02 15:26:02 +0000

    [MIG] l10n_be_intrastat_product: Migration to 18.0

--- l10n_be_intrastat_product/__manifest__.py
+++ l10n_be_intrastat_product/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Intrastat Product Declaration for Belgium",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Intrastat",
     "license": "AGPL-3",
     "summary": "Intrastat Product Declaration for Belgium",
